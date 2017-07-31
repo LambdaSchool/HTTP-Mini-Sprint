8bcc45b32027d2b840d3cf794f83339bbf944815
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-07-31 17:37:57 -0400

    working

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -3,5 +3,15 @@ import axios from 'axios';
 export const GET_FRIENDS = 'GET_FRIENDS';
 
 export const getFriends = () => {
-    // Add the code for this action
+    const promise = axios.get('http://localhost:5000/friends')
+    .then((res) => {
+        return res;
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+    return {
+        type: GET_FRIENDS,
+        payload: promise
+    };
 };
