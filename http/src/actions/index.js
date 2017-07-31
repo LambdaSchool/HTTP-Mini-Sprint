277b9a46153f9bab351d05cfbd5ec25a728a6667
mixelpixel <mixelpix@mac.com>
2017-07-31 17:48:47 -0400

    HTTP mini sprint √

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -2,6 +2,25 @@ import axios from 'axios';
 
 export const GET_FRIENDS = 'GET_FRIENDS';
 
+// // original
+// export const getFriends = () => {
+//     // Add the code for this action
+// };
+
+// // paste from README.md
+// export const getFriends = () => {
+//   const promise = axios.get('http://example.com');
+//   return {
+//       type: ACTION_TYPE,
+//       payload: promise
+//   };
+// };
+
+// Hint: The URL you want to be fetching from is http://localhost:5000/friends.
 export const getFriends = () => {
-    // Add the code for this action
+  const promise = axios.get('http://localhost:5000/friends');
+  return {
+      type: GET_FRIENDS,
+      payload: promise
+  };
 };
