4ecda74461b7040be1005e71f28d824e5be97f68
phytertek <ryan.phytertek@gmail.com>
2017-09-06 00:20:31 -0400

    Mini Sprint Complete

--- http/src/components/FriendsList.js
+++ http/src/components/FriendsList.js
@@ -1,37 +1,32 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { getFriends } from '../actions';
-import axios from 'axios';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getFriends } from '../actions'
 
 class FriendsList extends Component {
-    componentDidMount() {
-        this.props.getFriends();
-    }
+  componentDidMount() {
+    this.props.getFriends()
+  }
 
-    render() {
-        return (
-            <div>
-                <ul>
-                    {this.props.friends.map((friend, i) => {
-                        return (
-                            <li key={i}>
-                                <p>{`Friend ${i+1}`}</p>
-                                <p>{`Name: ${friend.name}`}</p>
-                                <p>{`Age: ${friend.age}`}</p>
-                                <p>{`Email: ${friend.email}`}</p>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.props.friends.map((friend, i) => (
+            <li key={i}>
+              <p>{`Friend ${i+1}`}</p>
+              <p>{`Name: ${friend.name}`}</p>
+              <p>{`Age: ${friend.age}`}</p>
+              <p>{`Email: ${friend.email}`}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        friends: state.friends
-    };
-};
+const mapStateToProps = (state) => ({
+  friends: state.friends
+})
 
-export default connect(mapStateToProps, { getFriends })(FriendsList);
+export default connect(mapStateToProps, { getFriends })(FriendsList)
