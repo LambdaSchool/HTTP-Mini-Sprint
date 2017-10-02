e0325b9abcad3ab5eaabba489203e6ca8d4ab47e
Christopher Atoki <kingatoki@Christophers-MacBook-Air.local>
2017-10-02 18:53:02 -0400

    HTTP Mini

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -1,7 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const GET_FRIENDS = 'GET_FRIENDS';
+export const GET_FRIENDS = "GET_FRIENDS";
 
 export const getFriends = () => {
-    // Add the code for this action
+  // Add the code for this action
+  const action = () => {
+    const promise = axios.get("http://localhost:5000/friends");
+    return {
+      type: GET_FRIENDS,
+      payload: promise
+    };
+  };
 };
--- http/src/index.js
+++ http/src/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import ReduxPromise from 'redux-promise';
-import reducers from './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import ReduxPromise from "redux-promise";
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+  <Provider
+    store={createStoreWithMiddleware(
+      reducers,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
