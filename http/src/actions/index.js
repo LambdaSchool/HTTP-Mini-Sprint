39db90b0ca27f9b67ec594862091e28953ba9e97
Alex Cassell <alex@simulism.net>
2017-11-01 14:53:25 -0400

    working as intended

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -4,4 +4,13 @@ export const GET_FRIENDS = 'GET_FRIENDS';
 
 export const getFriends = () => {
     // Add the code for this action
+    const friendsEndpoint = 'http://localhost:5000/friends'; //endpoint is a specific url on server
+    const friends = axios.get(friendsEndpoint);
+
+    console.log('friends', friends);
+
+    return {
+        type: GET_FRIENDS,
+        payload: friends,
+    };
 };
