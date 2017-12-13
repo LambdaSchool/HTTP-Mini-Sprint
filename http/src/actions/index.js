0ab0f315bedd70cdeb5a193d3f4ab15a3b547e16
JacquelynnRohrich <33401573+JacquelynnRohrich@users.noreply.github.com>
2017-12-13 14:59:29 -0600

    tada

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -3,5 +3,28 @@ import axios from 'axios';
 export const GET_FRIENDS = 'GET_FRIENDS';
 
 export const getFriends = () => {
-    // Add the code for this action
+
+
+    const apiUrl= 'http://localhost:5000/friends';
+    const friendsRequest = axios.get(apiUrl);
+    
+
+    return {
+        type: GET_FRIENDS,
+        payload: friendsRequest,
+    };
 };
+
+// enter redux-promise and axios
+// http < is stateless
+// most operations are asynchronous
+// promise: sibstitue for callbacks
+
+
+// var promise = new Promise;
+
+// promise
+// .then(function(results) {
+// })
+// .catch(function(error)  {    //handle that error
+// });
