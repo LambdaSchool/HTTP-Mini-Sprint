69072ebf8c6d5bfb4ee2c00ee74357c13559a139
ubuntu <irishseanvaldivia@gmail.com>
2017-12-17 13:38:28 -0600

    Another commit.

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -3,5 +3,9 @@ import axios from 'axios';
 export const GET_FRIENDS = 'GET_FRIENDS';
 
 export const getFriends = () => {
+    return {
+        type: GET_FRIENDS,
+        payload: 
+    };
     // Add the code for this action
 };
