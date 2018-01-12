58d2c65aa8406eeb31e1bb6ea64b4a0caffd5bad
Mark Marchant <jtla3@yahoo.com>
2018-01-11 23:54:46 -0800

    Round 2 of doing the project

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -4,4 +4,25 @@ export const GET_FRIENDS = 'GET_FRIENDS';
 
 export const getFriends = () => {
     // Add the code for this action
+    const apiUrl= 'http://localhost:5000/friends';
+    const friendRequest = axios.get(apiUrl)
+    console.log('actions', friendRequest);
+
+    // redux-promise middleware takes care of resolving the promise  
+    return {
+        type: GET_FRIENDS,
+        payload: friendRequest
+    };
 };
+
+// enter redux-promise and axios
+
+// http < is stateless --> server does not know who you are
+// http methods are: GET, POST, PUT, and DELETE 
+// GET = client wants information from server
+// POST = create a resource on the server
+// PUT = modify information on the server
+// DELETE = remove information from the server
+// most operations are asynchronous
+// Promise: substitue for callbacks
+// .then() --> promise succeeded .catch() --> handle that error
