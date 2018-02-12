416d028f8d69bbd2e8c8593252f0f706b5dac71b
Glenn-David Daniel <d2rd@outlook.com>
2018-02-12 12:38:32 -0800

    WIP morning lesson.  Debugged

--- client/src/components/FriendsList.js
+++ client/src/components/FriendsList.js
@@ -3,50 +3,49 @@ import axios from 'axios';
 
 class FriendsList extends Component {
   state = {
-    friends: []
+    friends: [],
+    loading: true,
+    noData: true,
   };
-  
-  render() {
-    if (this.state.loading && this.state.noData) {
-      // will this run
-    }
 
-    return (
-      <div>
+render(){
+
+  return (
+    <div>
         <div className="friend-title">Lambda Friends</div>
         {this.state.loading && <div>Loading Friends...</div>}
 
         {!this.state.loading && (
-          <ul className="friend-grid">
-            {this.state.friends.map(friend => {
-              return (
-                <li key={friend.id} className="friend">
-                  <div className="friend-name">{friend.name}</div>
-                  <div className="friend-age">{`Age: ${friend.age}`}</div>
-                  <div className="friend-email">{`Email: ${friend.email}`}</div>
-                </li>
-              );
-            })}
-          </ul>
-        )};  
+        <ul className="friend-grid">
+        {this.state.friends.map(friend => {
+        return (
+            <li key={friend.id} className="friend">
+            <div className="friend-name">{friend.name}</div>
+            <div className="friend-age">{`Age: ${friend.age}`}</div>
+            <div className="friend-email">{`Email: ${friend.email}`}</div>
+            </li>
+          );
+          })}
+        </ul>
+      )} 
     </div>
-    
-    )}
+  );
+}
 
 componentDidMount() {
-  axios
-    .get('http://localhost:5000/friends')
-    .then(response => {
-      // console.log('data', response.data);
-      this.setState({ friends: response.data});
+	this.setState({ loading: true });
+	axios
+	.get('http://localhost:5000/friends')
+	.then(response => {
+	this.setState({ friends: response.data, loading: false});
     })
-    .catch(error => {
-      console.log('there was an error', error);
-    })};
-
+	.catch(error => {
+	this.SetState({loading: false});
+	console.log('there was an error', error);
+});
+}
 }
 
 
 export default FriendsList;
-
-// http://localhost:5000/friends  6000 is reserved don't use change to 5000
+// http://localhost:5000/friends 6000 is reserved don't use change to 5000
