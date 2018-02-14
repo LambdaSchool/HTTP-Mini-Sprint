b87df8471c50c86979ac3916b8b1a848a7c3a76e
Charlie Sparks <csparks@antioch.edu>
2018-02-13 17:17:29 -0800

    working project, no issues

--- client/src/components/FriendsList.js
+++ client/src/components/FriendsList.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class FriendsList extends Component {
+  state = {
+    friends: []
+  }
+
   render() {
     return (
       <div>
@@ -19,6 +23,14 @@ class FriendsList extends Component {
         </ul>
       </div>
     );
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/friends")
+      .then(response => {
+        this.setState({ friends: response.data });
+      })
+      .catch(error => console.log("there was an error"));
   }
 }
 
