dce0f1b13eae3294a048057cb084aaf8ce01732e
Victor Montoya <victoramontoya@gmail.com>
2018-04-09 21:11:22 -0700

    Victor Finished

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import './index.css';
 import FriendsList from './components/FriendsList';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
-ReactDOM.render(<FriendsList />, document.getElementById('root'));
+
+ReactDOM.render(
+    <Router>
+        <div>
+            <h2>Friends List</h2>
+            <Route exact path="/friends" component={FriendsList} />
+            {/* <Route path="/friends/:id" component={friend} /> */}
+        </div>
+    </Router>,
+    document.getElementById('root')
+);
