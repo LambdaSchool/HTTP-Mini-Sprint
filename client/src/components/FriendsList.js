36eb7867fd9cdc7b7566e3072ef16421f1019eb2
JohnCoronel <john.m.coronel@gmail.com>
2018-04-09 20:56:17 -0700

    John Coronel - mini finished

--- client/src/components/FriendsList.js
+++ client/src/components/FriendsList.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class FriendsList extends Component {
+constructor(props){
+  super(props);
+  this.state = {
+    friends: []
+  }
+}
+componentDidMount(){
+  axios.get('http://localhost:5000/friends')
+  .then( response => {
+    this.setState({friends:response.data})
+  })
+}
+
   render() {
     return (
       <div>
