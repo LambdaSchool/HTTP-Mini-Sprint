328cb6e5f56a72263556d3fd1727163b81a2a7cb
unknown <alexisreyes90@gmail.com>
2018-04-09 23:21:05 -0400

    Alexis Reyes: HTTP-Minis-Sprint complete

--- client/src/components/FriendsList.js
+++ client/src/components/FriendsList.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class FriendsList extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      frands: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+    .then(response => {
+      this.setState(() => ({frands: response.data}));
+    })
+  }
+
   render() {
     return (
       <div>
-        <div className="friend-title">Lambda Friends</div>
+        <div className="friend-title">Lambda Frands</div>
         <ul className="friend-grid">
-          {this.state.friends.map(friend => {
+          {this.state.frands.map(friend => {
             return (
               <li key={friend.id} className="friend">
                 <div className="friend-name">{friend.name}</div>
