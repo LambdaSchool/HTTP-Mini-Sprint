6dcf87167daee22b641feee568c7f437479223ea
ubuntu <irishseanvaldivia@gmail.com>
2018-04-17 13:37:58 -0500

    Typed in code by hand. Now to go through and get it working, and then make more comments on what I don't understand...

--- client/src/components/FriendsList.js
+++ client/src/components/FriendsList.js
@@ -27,6 +27,40 @@ export default class FriendsList extends Component {
       })
       .catch(err => console.log(err))
   }
+
+  nameChange = (e) => { this.setState({name: e.target.value})}
+  ageChange = (e) => { this.setState({ age: e.target.value })}
+  emailChange = (e) => { this.setState({ email: e.target.value })}
+  buttonSubmit = () => {
+    const { name, age, email } = this.state
+    axios.post('http://localhost:5000/friends', { name, age, email})
+      .then( (response) => {
+        this.setState({ friends: response.data, name: '', age: '', email: ''})
+      })
+  }
+  render() {
+    return (
+      <div>
+        <h1>Friends:</h1>
+        <ul className='friends'>
+          {this.state.friends.map( friend => {
+            return (
+              <li key={friend.id} className='friend'>
+                <p>Name: {friend.name}</p>
+                <p>Age: {friend.age}</p>
+                <p>Email: {friend.email}</p>
+              </li>
+            )
+          })}
+        </ul>
+        <h3>Add Friend</h3>
+        <input type='text' placeholder='Name' onChange={this.nameChange} value={this.state.name}/>
+        <input type='number' placeholder='Age' onChange={this.ageChange} value={this.state.age}/>
+        <input type='text' placeholder='Email' onChange={this.emailChange} value={this.state.email}/>
+        <button onClick={this.buttonSubmit}>Submit</button>
+      </div>
+    )
+  }
 }
 
 export default FriendsList;
