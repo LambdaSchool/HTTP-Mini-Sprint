a9d36af5f3f240d4fb59401f4d1426809bc7cbae
ubuntu <irishseanvaldivia@gmail.com>
2018-04-17 13:07:38 -0500

    Following with the solution on Piazza after watching video.

--- client/src/components/FriendsList.js
+++ client/src/components/FriendsList.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './Friends.css';
 
-class FriendsList extends Component {
-  render() {
-    return (
-      <div>
-        <div className="friend-title">Lambda Friends</div>
-        <ul className="friend-grid">
-          {this.state.friends.map(friend => {
-            return (
-              <li key={friend.id} className="friend">
-                <div className="friend-name">{friend.name}</div>
-                <div className="friend-age">{`Age: ${friend.age}`}</div>
-                <div className="friend-email">{`Email: ${friend.email}`}</div>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
+export default class FriendsList extends Component {
+  constructor(){
+    super()
+    this.state = {
+      friends: [],
+      name: '',
+      age: '',
+      email: ''
+    }
   }
 }
 
